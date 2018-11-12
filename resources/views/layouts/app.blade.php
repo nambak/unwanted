a67a3a4bbbe8427a0dc5cd70d760ee44368da354
남성룡 <alex.nam@azit501.com>
2018-11-12 23:57:59 +0900

    add google adsense script

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,6 +18,15 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    <!-- Google AdSense -->
+    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    <script>
+        (adsbygoogle = window.adsbygoogle || []).push({
+            google_ad_client: "ca-pub-3760455502657641",
+            enable_page_level_ads: true
+        });
+    </script>
 </head>
 <body>
     <div id="app">
