40dc66da7645019a7fef8eaedb36a3fd25f3d8c5
Alex Nam <nambak80@gmail.com>
2018-11-15 21:38:16 +0900

    fix issue #1 (#8)

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -62,7 +62,7 @@
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
+                                        로그아웃
                                     </a>
 
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
