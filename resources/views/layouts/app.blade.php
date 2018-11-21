a00302669e1b9ec5f9830e868765dd1e07384371
Alex Nam <nambak80@gmail.com>
2018-11-21 17:53:42 +0900

    add google search console meta tag (#16)

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <meta name="google-site-verification" content="zCBs9PKVOsp3auMGZ6AuYQDw19pLBwsT4707c1ME7ao" />
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
