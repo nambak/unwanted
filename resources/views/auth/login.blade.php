94c11601fbeebd7f06a739ed94ba61227090cb2c
Alex Nam <nambak80@gmail.com>
2019-03-22 15:47:41 +0900

    Issue 18 (#35)
    * disqus install
    
    * login view, send reset email view css fixed

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,71 +1,67 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">로그인</div>
+    <div class="card">
+        <header class="card-header">
+            <p class="card-header-title">로그인</p>
+        </header>
+        <div class="card-content">
+            <form method="POST" action="{{ route('login') }}">
+                @csrf
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+                <div class="field">
+                    <label for="email" class="label">이메일</label>
+                    <div class="control">
+                        <input id="email" type="email"
+                               class="input {{ $errors->has('email') ? ' is-danger' : '' }}" name="email"
+                               value="{{ old('email') }}" required autofocus>
 
-                        <div class="form-group row">
-                            <label for="email" class="col-sm-4 col-form-label text-md-right">이메일</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
-
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif</div>
-
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">비밀번호</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        아이디 저장
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    로그인
-                                </button>
-
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    비밀번호 찾기
-                                </a>
-                            </div>
-                        </div>
-                    </form>
+                        @if ($errors->has('email'))
+                            <p class="help is-danger">
+                                <strong>{{ $errors->first('email') }}</strong>
+                            </p>
+                        @endif</div>
                 </div>
-            </div>
+
+                <div class="field">
+                    <label for="password" class="label">비밀번호</label>
+                    <div class="control">
+                        <input id="password" type="password"
+                               class="input {{ $errors->has('password') ? ' is-danger' : '' }}"
+                               name="password"
+                               required>
+
+                        @if ($errors->has('password'))
+                            <p class="help is-danger">
+                                <strong>{{ $errors->first('password') }}</strong>
+                            </p>
+                        @endif
+                    </div>
+                </div>
+
+                <div class="field">
+                    <div class="control">
+                        <label class="checkbox" for="remember">
+                            <input type="checkbox" name="remember"
+                                   id="remember" {{ old('remember') ? 'checked' : '' }}>
+                            아이디 저장
+                        </label>
+                    </div>
+                </div>
+
+                <div class="field">
+                    <div class="control">
+                        <button type="submit" class="button">
+                            로그인
+                        </button>
+                    </div>
+                </div>
+                <div class="field has-text-right">
+                    <a href="{{ route('password.request') }}">
+                        비밀번호 찾기
+                    </a>
+                </div>
+            </form>
         </div>
     </div>
-</div>
 @endsection
--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,47 +1,45 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
+    <div class="card">
+        <header class="card-header">
+            <p class="card-header-title">{{ __('Reset Password') }}</p>
+        </header>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    <form method="POST" action="{{ route('password.email') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
-
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+        <div class="card-content">
+            @if (session('status'))
+                <div class="notification is-success">
+                    {{ session('status') }}
                 </div>
-            </div>
+            @endif
+
+            <form method="POST" action="{{ route('password.email') }}">
+                @csrf
+
+                <div class="field">
+                    <label for="email" class="label">{{ __('E-Mail Address') }}</label>
+
+                    <div class="control">
+                        <input id="email" type="email"
+                               class="input {{ $errors->has('email') ? 'is-danger' : '' }}" name="email"
+                               value="{{ old('email') }}" required>
+
+                        @if ($errors->has('email'))
+                            <p class="help is-danger">
+                                <strong>{{ $errors->first('email') }}</strong>
+                            </p>
+                        @endif
+                    </div>
+                </div>
+
+                <div class="field">
+                    <div class="control">
+                        <button type="submit" class="button">
+                            {{ __('Send Password Reset Link') }}
+                        </button>
+                    </div>
+                </div>
+            </form>
         </div>
     </div>
-</div>
 @endsection
