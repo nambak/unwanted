09a40903776f00aae86b6066d93a3d97b21c8261
Alex Nam <nambak80@gmail.com>
2025-09-09 10:33:02 +0900

    :heavy_minus_sign: Bootstrap 및 관련 의존성 제거, Tailwind CSS 설정 업데이트

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,14 @@
 module.exports = {
     content: [
+        './resources/views/**/*.blade.php',
+        './resources/js/**/*.{js,ts,vue}',
         './node_modules/vue-tailwind-wysiwyg-editor/**/*.{js,ts,vue}',
     ],
+    theme: {
+        extend: {
+            fontFamily: {
+                'sans': ['Nanum Gothic', 'sans-serif'],
+            },
+        },
+    },
 }
